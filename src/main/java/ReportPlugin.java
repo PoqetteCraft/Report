06744e1bbcf72faa68b623b1aa6e70fc1ddf2c01
Adam <afranklint16@gmail.com>
2018-04-03 20:29:30 -0700

    Create plugin.yml, config.yml, and main plugin class

---
+++ src/main/java/ReportPlugin.java
@@ -0,0 +1,16 @@
+import com.github.pocketkid2.database.Database;
+import com.github.pocketkid2.database.DatabasePlugin;
+
+public class ReportPlugin extends DatabasePlugin {
+
+	@Override
+	public void onEnable() {
+		Database.register(this);
+		getLogger().info("Done!");
+	}
+
+	@Override
+	public void onDisable() {
+		getLogger().info("Done!");
+	}
+}
