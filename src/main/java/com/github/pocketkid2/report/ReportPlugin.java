51da716c1e7124a9c04c31d1441bba2cf2805123
Adam <afranklint16@gmail.com>
2018-04-12 16:28:15 -0700

    Update main plugin

--- src/main/java/com/github/pocketkid2/report/ReportPlugin.java
+++ src/main/java/com/github/pocketkid2/report/ReportPlugin.java
@@ -5,14 +5,36 @@ import com.github.pocketkid2.database.DatabasePlugin;
 
 public class ReportPlugin extends DatabasePlugin {
 
+	private ReportManager manager;
+
+	private Settings settings;
+
 	@Override
 	public void onEnable() {
-		Database.register(this);
-		getLogger().info("Done!");
+		saveDefaultConfig();
+		settings = new Settings(getConfig());
+		getCommand("report").setExecutor(new ReportCommand(this));
+		manager = new ReportManager(this);
+
+		if (Database.register(this)) {
+			manager.initStatements();
+			manager.initTable();
+			getLogger().info("Done!");
+		} else {
+			getLogger().warning("Finished registering but currently disabled. Plugin will come online when the database connection is re-established.");
+		}
 	}
 
 	@Override
 	public void onDisable() {
 		getLogger().info("Done!");
+	}
+
+	public ReportManager getReportManager() {
+		return manager;
+	}
+
+	public Settings getSettings() {
+		return settings;
 	}
 }
